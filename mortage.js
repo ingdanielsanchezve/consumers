b6f418f1b0b8d105bc827ff89a774943ac96d008
Daniel Sánchez <ing.danielsanchez@outlook.com>
2018-08-24 18:27:45 -0400

    fields validation

--- mortage.js
+++ mortage.js
@@ -3,14 +3,11 @@
 
     var years_range = document.getElementById("years_mortage_range");
     var years_mortage = document.getElementById("years_mortage");
-
     var interest_range = document.getElementById("interest_rate_range");
     var interest_rate = document.getElementById("interest_rate");
-
     var loan_amount = document.getElementById("loan_amount");
     var annual_tax = document.getElementById("annual_tax");
     var annual_insurance = document.getElementById("annual_insurance");
-
     var mortageBtn = document.getElementById("calcMortageBtn");
 
     function checkMobile() {        
@@ -18,10 +15,11 @@
     }    
 
     if (checkMobile()){
-        document.getElementById("resultsPanel").style.maxHeight = "0";
-        document.getElementById("resultsPanel").style.overflow = "hidden";
-        document.getElementById("resultsPanel").style.transition = "max-height 2s linear";
-        document.getElementById("resultsPanel").style.display = "none";
+        var resultsPanel = document.getElementById("resultsPanel");
+            resultsPanel.style.maxHeight = "0";
+            resultsPanel.style.overflow = "hidden";
+            resultsPanel.style.transition = "max-height 2s linear";
+            resultsPanel.style.display = "none";
     }
 
     function showInterest(){
@@ -63,66 +61,68 @@
         }
     }
 
-    function showResults(element, value, isMobile){
+    function showResults(elementId, value, isMobile){
+
+        field = document.getElementById(elementId);
 
         if (isMobile) {
-            document.getElementById(element).className = "results_value_display_mobile";
-            if (element == "mortage_value_mobile"){ 
-                document.getElementById(element).style.fontSize = "18px"
-                document.getElementById(element).style.marginTop = "19px"
+            field.className = "results_value_display_mobile";
+            if (elementId == "mortage_value_mobile"){ 
+                field.style.fontSize = "18px"
+                field.style.marginTop = "19px"
             }
             animatePanel();
         }else{
-            document.getElementById(element).className = "results_value_display";
-            if (element == "mortage_value")
-                document.getElementById(element).style.fontSize = "22px"            
+            field.className = "results_value_display";
+            if (elementId == "mortage_value")
+                field.style.fontSize = "22px"            
         }
-        document.getElementById(element).innerHTML = "$ " + value.toFixed(2);
+        field.innerHTML = "$ " + value.toFixed(2);
     }
 
     function validateFields(){
+
         var fields = ["loan_amount", "annual_tax", "annual_insurance"];        
         var errors = ["Loan Amount is mandatory", "Annual Tax is mandatory", "Annual Insurance is mandatory"];
         var error_labels = ["loan_amount_err_label", "annual_tax_err_label", "annual_insurance_err_label"];
         var valid = true;
 
         for (var i = 0; i < fields.length; i++) {
-            if (document.getElementById(fields[i]).value == "") {
-                document.getElementById(fields[i]).style.border = "solid 1px #da3535";
+            var field = document.getElementById(fields[i])
+            if (field.value == "") {
+                field.style.border = "solid 1px #da3535";
                 document.getElementById(error_labels[i]).innerHTML = (checkMobile()) ? 'Mandatory filed' : errors[i];
                 valid = false;
             }
         }
 
-        if (document.getElementById("years_mortage").value == "" || document.getElementById("years_mortage").value == "0") {
-            document.getElementById("monthly_err_label").innerHTML = "Invalid Years of Mortage";
-            document.getElementById("monthly_err_label").style.display = "block";
-            document.getElementById("monthly_err_label").style.marginTop = "15px";            
-            valid = false;
-        }else{
-            document.getElementById("monthly_err_label").innerHTML = "";
-            document.getElementById("monthly_err_label").style.display = "none";
-            document.getElementById("monthly_err_label").style.marginTop = "0px";
-            valid = true;
+        var ranges = ["years_mortage", "interest_rate"];
+        var ranges_errors_msg = ["Invalid Years of Mortage", "Invalid Rate of Interest"];
+        var err_label = document.getElementById("monthly_err_label");
+
+        for (var i = 0; i < ranges.length; i++) {
+
+            var rangeField = document.getElementById(ranges[i]);
+            
+            if (rangeField.value == "" || rangeField.value == "0") {
+                err_label.innerHTML = ranges_errors_msg[i];
+                err_label.style.display = "block";
+                err_label.style.marginTop = "15px";            
+                valid = false;
+            }else{
+                err_label.innerHTML = "";
+                err_label.style.display = "none";
+                err_label.style.marginTop = "0px";
+                valid = true;
+            }
         }
-        if (document.getElementById("interest_rate").value == "" || document.getElementById("interest_rate").value == "0") {
-            document.getElementById("monthly_err_label").innerHTML = "Invalid Rate of Interest";
-            document.getElementById("monthly_err_label").style.display = "block";
-            document.getElementById("monthly_err_label").style.marginTop = "15px";            
-            valid = false;
-        }else{
-            document.getElementById("monthly_err_label").innerHTML = "";
-            document.getElementById("monthly_err_label").style.display = "none";
-            document.getElementById("monthly_err_label").style.marginTop = "0px";
-            valid = true;
-        }        
             
         return valid;
     }
 
     function calculateMortage(){
 
-        if (!validateFields()){
+        if (!validateFields){
              return;
         }else{
 
@@ -160,10 +160,13 @@ function checkMobile() {
     return (/(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|ipad|iris|kindle|Android|Silk|lge |maemo|midp|mmp|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows (ce|phone)|xda|xiino/i.test(navigator.userAgent) || /1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-/i.test(navigator.userAgent.substr(0, 4))) ? true : false;
 }    
 
-function validateFields(field) {
+function validateFields(id) {
     var error_label;
     var error;
-    switch (field) {
+    var field = document.getElementById(id);
+    var calcMortageBtn = document.getElementById("calcMortageBtn");
+
+    switch (id) {
         case 'loan_amount':
             error_label = 'loan_amount_err_label';
             error = 'Loan Amount is mandatory'
@@ -177,13 +180,16 @@ function validateFields(field) {
             error = 'Annual Insurance is mandatory'
             break;
     }
-    if (document.getElementById(field).value == "") {
-        document.getElementById(field).style.border = "solid 1px #da3535";
-        document.getElementById(error_label).innerHTML = (checkMobile()) ? 'Mandatory filed' : error;
-        document.getElementById("calculateMortage").disabled = true;
+
+    var label = document.getElementById(error_label);
+
+    if (field.value == "") {
+        field.style.border = "solid 1px #da3535";
+        label.innerHTML = (checkMobile()) ? 'Mandatory filed' : error;
+        calcMortageBtn.disabled = true;
     } else {
-        document.getElementById(field).style.border = "solid 1px #a6a6a6";
-        document.getElementById(error_label).innerHTML = "";
-        document.getElementById("calculateMortage").disabled = false;
+        field.style.border = "solid 1px #a6a6a6";
+        label.innerHTML = "";
+        calcMortageBtn.disabled = false;
     }
 }
